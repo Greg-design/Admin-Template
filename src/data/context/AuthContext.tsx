f83de03391b8885eb4d2ff6d28ea2de253337544
Greg-design <gregory_gr@outlook.com>
2023-03-30 16:56:17 -0300

    sessão com cookies

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,10 +1,11 @@
 import Usuario from "@/model/Usuario";
+import Cookies from "js-cookie";
 import route from "next/router";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import firebase from "../../firebase/config";
 
 interface AuthContextProps {
-  usuario?: Usuario;
+  usuario?: Usuario | null;
   loginGoogle?: () => Promise<void>;
 }
 
@@ -25,18 +26,46 @@ async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuar
   };
 }
 
+function gerenciarCookie(logado: boolean) {
+  if (logado) {
+    Cookies.set("admin-template-auth", "true", {
+      expires: 7,
+    });
+  } else {
+    Cookies.remove("admin-template-auth");
+  }
+}
+
 export function AuthProvider({ children }: any) {
-  const [usuario, setUsuario] = useState<Usuario>();
+  const [carregando, setCarregando] = useState(true);
+  const [usuario, setUsuario] = useState<Usuario | null>(null);
+
+  async function configurarSessao(usuarioFirebase: any) {
+    if (usuarioFirebase?.email) {
+      const usuario = await usuarioNormalizado(usuarioFirebase);
+      setUsuario(usuario);
+      gerenciarCookie(true);
+      setCarregando(false);
+      return usuario.email;
+    } else {
+      setUsuario(null);
+      gerenciarCookie(false);
+      setCarregando(false);
+      return false;
+    }
+  }
 
   async function loginGoogle() {
     const res = await firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
 
-    if (res.user?.email) {
-      const usuario = await usuarioNormalizado(res.user);
-      setUsuario(usuario);
-      route.push("/");
-    }
+    configurarSessao(res.user);
+    route.push("/");
   }
+
+  useEffect(() => {
+    const cancelar = firebase.auth().onIdTokenChanged(configurarSessao);
+    return () => cancelar();
+  }, []);
 
   return (
     <AuthContext.Provider
